8a51e1cccd33b396087e54e0f1f3ff4b098faf91
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-08-07 05:48:58 +0100

    fix/add modified files

--- src/components/Recipe1.js
+++ src/components/Recipe1.js
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import '../App.css';
 
-
 export default  function Recipes(){
 
     const [recipes, setRecipes] = useState([
@@ -11,11 +10,9 @@ export default  function Recipes(){
 
     const [header, setHeader] = useState(recipes[0]);
     
-    function handleButtonClick(recipe){
-        setHeader(recipe)
-    }
+    
 
-    const [ingredients1, setIngredients1] = useState(['2 tablespoons butter',
+    const ingredients1 = ['2 tablespoons butter',
     '2 cloves garlic, minced',
     '1 cup heavy cream',
     '3/4 teaspoon salt',
@@ -24,11 +21,47 @@ export default  function Recipes(){
     '3/4 pound fusilli',
     '1/2 cup grated Parmesan cheese',
     '2 tablespoons chopped chives, scallion tops, or parsley'
-]);
+];
 
-    const [direction1, setDirection1] = useState(['In a medium saucepan, melt the butter over moderately low heat. Add the garlic and cook for 30 seconds. Stir in the cream, salt, pepper, and artichoke hearts. Cook until just heated through, about 3 minutes.',
+    const direction1 = ['In a medium saucepan, melt the butter over moderately low heat. Add the garlic and cook for 30 seconds. Stir in the cream, salt, pepper, and artichoke hearts. Cook until just heated through, about 3 minutes.',
     'In a large pot of boiling, salted water, cook the fusilli until just done, about 13 minutes. Drain the pasta and toss with the cream sauce, Parmesan, and chives.'
-]);
+];
+
+  
+        const ingredientsHTML = ingredients1.map((ingredient, index) => (
+            `<li key=${index}>${ingredient}</li>`
+        )).join('');
+
+        const directionsHTML = direction1.map((step, index) => (
+            `<li key=${index}>${step}</li>`
+        )).join('');
+
+        const [content, setContent] = useState(`
+            <h2>Ingredients</h2>
+            <ul>${ingredientsHTML}</ul>
+            <h2>Directions</h2>
+            <ol>${directionsHTML}</ol>
+        `);
+
+    const ingredient2 = ['3 tablespoons butter',
+    '1 teaspoon seasoning salt',
+    '1 teaspoon onion powder',
+    '4 skinless, boneless chicken breast halves',
+    '2 teaspoons garlic powder']
+
+    const direction = [
+        'Melt butter in a large skillet over medium high heat.',
+        'Add chicken and sprinkle with garlic powder, seasoning salt and onion powder.',
+        'Saute about 10 to 15 minutes on each side, or until chicken is cooked through and juices run clear.'
+    ]
+
+
+    function handleButtonClick(recipe){
+        setHeader(recipe)
+        if(header==='Artichoke Pasta'){
+                
+        }
+    }
 
     return (
         <div>
@@ -47,13 +80,8 @@ export default  function Recipes(){
         <div className="recipe-header" >
         <h1>{header}</h1>
         </div>
-        <div>
-            <textarea
-            rows={10}
-            cols={87}
-            >
+        <div className="contents"  dangerouslySetInnerHTML={{__html:content}} >
             
-            </textarea>
         </div>
         </div>
     )
