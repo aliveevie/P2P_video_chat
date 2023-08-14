3018ac51859de53aa53bef78239e3e1039cfc8fa
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-08-14 10:28:05 +0100

    modified files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import './App.css';
-import { useState } from 'react';
-import Headers from './components/Header';
-import Recipes from './components/Recipe1';
-
+import { useState, useRef, } from 'react';
 
 function App() {
+
+  
+  const videoElement = useRef(null);
+
+  useState(() => {
+    const alert = window.alert('Please allow this computer to use your camera')
+    if(alert){
+      if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+        
+       const constraints = { video: true, audio: true}
+
+       // Access the user's webcam
+       navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        videoElement = window.getElementById('video')
+        
+        // set the stream as the video element source
+        videoElement.srcObject = stream
+
+        // Play the video
+        videoElement.play();
+
+       })
+       .catch((e) => console.log('Error accessing the webcam'))
+
+      }
+
+    }
+  })
   return (
     <div className='container' >
-     <Headers />
-     <Recipes />
+   <div id="video" ></div>
     </div>
   );
 }
